c8dfea701f5e1b7d403c19ff4f0b21ef0a255b3c
Baptiste Harbuz <baptiste.harbuz@yahoo.com>
2024-06-28 14:31:08 +0200

    New contact page and style fix

--- src/Pages/ContactPage.jsx
+++ src/Pages/ContactPage.jsx
@@ -1,44 +1,49 @@
 import React from 'react';
 import '../Styles/Contact.css'
+import { BsFillTelephoneFill, BsEnvelope, BsLinkedin, BsGithub } from "react-icons/bs";
 
 const contact = () => {
 
     return (
         <div className='contact-container'>
-            <div className='txt-contact'>
+            <section>
                 <h1 className='h1-contact'>contact</h1>
                 <div className='line'></div>
-                <div className='number-container'>
-                    <a className='number' href="tel:0618125330">06 18 12 53 30</a>
-                </div>
-                <div className='mail-container' onClick={() => window.location.href = "mailto:contact@baptisteharbuz.fr"}>
-                    <h3 className='mail'>contact@baptisteharbuz.fr</h3>
-                    <h6 className='contact-click'>Click !</h6>
-                </div>
-                <a className='linkedin-container' href="https://www.linkedin.com/in/baptiste-harbuz/" target="_blank" rel="noopener noreferrer">
-                    <div className='container-details'>
-                        <div className='logo-linkedin'></div>
-                        <h3 className='linkedin'>LinkedIn</h3>
+                <div className="contact-container">
+                    <a href="tel:0618125330" className="contact-item">
+                        <BsFillTelephoneFill className='icons' />
+                        <h2>06 18 12 53 30</h2>
+                        <h6 className='contact-click'>Click !</h6>
+                    </a>
+                    <div className='contact-item' onClick={() => window.location.href = "mailto:contact@baptisteharbuz.fr"}>
+                        <BsEnvelope className='icons' />
+                        <h2 className='mail'>contact@baptisteharbuz.fr</h2>
                         <h6 className='contact-click'>Click !</h6>
                     </div>
-                </a>
-                <a className='github-container' href="https://github.com/baptisteharbuz" target="_blank" rel="noopener noreferrer">
-                    <div className='container-details'>
-                        <div className='logo-github'></div>
-                        <h3 className='github'>GitHub</h3>
+                    <a href="https://www.linkedin.com/in/baptiste-harbuz/" target="_blank" rel="noopener noreferrer" className="contact-item">
+                        <BsLinkedin className='icons' />
+                        <h2 className='linkedin'>LinkedIn</h2>
                         <h6 className='contact-click'>Click !</h6>
-                    </div>
-                </a>
+                    </a>
+                    <a href="https://github.com/baptisteharbuz" target="_blank" rel="noopener noreferrer" className="contact-item">
+                        <BsGithub className='icons' />
+                        <h2 className='github'>GitHub</h2>
+                        <h6 className='contact-click'>Click !</h6>
+                    </a>
+                </div>
                 <div className='txt-presentation'>
-                    <p>🎨 Je suis passionné par l’art et le design graphique. La communication visuelle est pour moi essentielle. Logo pour restaurant, pochettes de singles, motion design pour présentation de marque, montage/cadrage de vidéo & clip.</p>
-
-                    <p>🎸 Mon amour pour la musique ma poussé dans un groupe qui me permet de déployer et d’accroître mes atouts tant sur l’aspect graphique et visuel que sur la production musicale et son interprétation scénique.</p>
-
-                    <p>💻 Le développement web n’est qu’une suite logique dans la continuité de mon aventure professionnelle et artistique. Je suis actuellement en formation Développeur Web & Web Mobile diplômante Bac + 2 à la ForEach Academy.</p>
-
-                    <p>🎨🎵💻 Je suis le créateur qui mélange les couleurs, compose les mélodies et écrit le code. Mon parcours est une quête pour unifier l'art, la musique et la technologie. Chaque jour est une nouvelle page blanche, une partition à interpréter et un site web à développer. Si vous partagez ma passion pour cette aventure multidimensionnelle, je serais ravi de vous rencontrer pour discuter de nos prochains chefs-d'œuvre en perspective 🚀</p>
+                    <span>🎨 Je suis passionné par l’art et le design graphique. La communication visuelle est pour moi essentielle. Logo pour restaurant, pochettes de singles, motion design pour présentation de marque, montage/cadrage de vidéo & clip.
+                        <br />
+                        <br />
+                        🎸 Mon amour pour la musique m'a poussé dans un groupe qui me permet de déployer et d’accroître mes atouts tant sur l’aspect graphique et visuel que sur la production musicale et son interprétation scénique.
+                        <br />
+                        <br />
+                        💻 Le développement web n’est qu’une suite logique dans la continuité de mon aventure professionnelle et artistique. Diplomé en Developpement Web & Web mobile (Bac + 2) et major de promotion à la ForEach Academy, je poursuis ma route en altenance depuis Juillet 2024 en Conception et Développement d'Application (Bac + 4).
+                        <br />
+                        <br />
+                        🎨🎵💻 Je suis le créateur qui mélange les couleurs, compose les mélodies et écrit le code. Mon parcours est une quête pour unifier l'art, la musique et la technologie. Chaque jour est une nouvelle page blanche, une partition à interpréter et un site web à développer 🚀</span>
                 </div>
-            </div>
+            </section>
         </div>
     )
 }
